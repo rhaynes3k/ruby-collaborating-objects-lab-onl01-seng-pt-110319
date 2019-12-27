590dbf461be28b4a8c18f99430b00550d5c20cc3
Rommel Haynes <rhaynes3k@gmail.com>
2019-12-27 20:47:06 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,15 @@
+require 'pry'
+class MP3Importer
+  attr_accessor :path, :files, :song
+  def initialize(path)
+    @path = path
+    @files = Dir.entries(path).select{|e|e.include?("-")}
+    #binding.pry
+  end
+  
+  def import
+    @files.map{|filename|Song.new_by_filename(filename)}
+    
+    #binding.pry
+  end
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,11 @@
 require 'pry'
 class Song
   attr_accessor :name, :artist
-
+  @@all = []
   @songs = []
   def initialize(name)
     @name = name
+    @@all << self
   end
   
   def artist= (artist)
@@ -16,4 +17,13 @@ class Song
     @songs << self
   end
   
+  def self.new_by_filename(filename)
+    name = filename.split("-")
+    song = self.new(name[1].strip)
+    Artist.find_or_create_by_name(name[0].strip)
+  end
+  
+  def self.all
+    @@all
+  end
 end
